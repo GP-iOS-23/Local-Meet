f43a4efcc55787095e11d308f2d23cc38c554d0f
Gleb <130483785+GP-iOS-23@users.noreply.github.com>
2026-02-12 22:01:36 +0500

    Initial Commit

---
+++ Local Meet/Local_MeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_MeetApp.swift
+//  Local Meet
+//
+//  Created by Глеб Поляков on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Local_MeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
